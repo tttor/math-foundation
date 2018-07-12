2aa468ffe4fd84dafd6ce63c4835b33ed984e85f
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-07-12 18:01:54 +1000

    fix

--- optim/numopt-nocedal-2006/script/test.py
+++ optim/numopt-nocedal-2006/script/test.py
@@ -8,11 +8,18 @@ def main():
     x_0 = torch.tensor([1.3, 0.7, 0.8, 1.9, 1.2])
     print('x_0=', x_0)
 
-    assert torch.allclose( util.rosenbrock(x_0), torch.tensor([sciopt.rosen(x_0.numpy())], dtype=torch.float32) )
+    assert torch.allclose(util.rosenbrock(x_0),
+                          torch.tensor([sciopt.rosen(x_0.numpy())], dtype=torch.float32))
     print('Test util.rosenbrock() vs scipy: OK')
 
-    assert torch.allclose(util.grad(util.rosenbrock, x_0), torch.from_numpy(sciopt.rosen_der(x_0.numpy())), rtol=1e-03, atol=1e-03)
+    assert torch.allclose(util.grad(util.rosenbrock, x_0),
+                          torch.from_numpy(sciopt.rosen_der(x_0.numpy())), rtol=1e-03, atol=1e-03)
     print('Test util.grad() vs scipy: OK')
+
+    v = torch.rand_like(x_0)
+    assert torch.allclose(util.hess_vec_proc(util.rosenbrock, x_0, v),
+                          torch.from_numpy(sciopt.rosen_hess_prod(x.numpy(), v.numpy())), rtol=1e-03, atol=1e-03)
+    print('Test util.hess_vec_prod() vs scipy: OK')
 
 if __name__ == '__main__':
     main()
--- optim/numopt-nocedal-2006/script/util.py
+++ optim/numopt-nocedal-2006/script/util.py
@@ -9,15 +9,15 @@ def grad(fn, _x):
     out.backward()
     return x.grad
 
-# def rosen_hess_vec_prod(_x, v):
-#     # https://discuss.pytorch.org/t/calculating-hessian-vector-product/11240
-#     x = Variable(_x.data.clone(), requires_grad=True)
-#     out = fn(x)
-#     grad_f, = torch.autograd.grad(out, x, create_graph=True)
-#     z = torch.dot(grad_f, v)
-#     z.backward()
-#     assert torch.allclose(x.grad, torch.from_numpy(sciopt.rosen_hess_prod(x.detach().numpy(), v.double())), rtol=1e-03, atol=1e-03)
-#     return x.grad
+def hess_vec_prod(fn, _x, v):
+    # https://discuss.pytorch.org/t/calculating-hessian-vector-product/11240
+    x = Variable(_x.data.clone(), requires_grad=True)
+    out = fn(x)
+    grad_f, = torch.autograd.grad(out, x, create_graph=True)
+    z = torch.dot(grad_f, v)
+    z.backward()
+    assert torch.allclose(x.grad, torch.from_numpy(sciopt.rosen_hess_prod(x.detach().numpy(), v.double())), rtol=1e-03, atol=1e-03)
+    return x.grad
 
 def rosenbrock(x):
     # https://en.wikipedia.org/wiki/Rosenbrock_function
