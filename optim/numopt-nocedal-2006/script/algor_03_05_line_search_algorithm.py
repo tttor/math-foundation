b537d99447b4df4790fdabf81066dca5f2c7bba0
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-07-13 10:03:53 +1000

    fix

--- optim/numopt-nocedal-2006/script/algor_03_05_line_search_algorithm.py
+++ optim/numopt-nocedal-2006/script/algor_03_05_line_search_algorithm.py
@@ -5,7 +5,12 @@ from algor_03_06_zoom import zoom
 
 def line_search_algorithm(fn, x_k, p_k, alpha_max):
     '''
+    Algorithm 3.5 (Line Search Algorithm), p60
+
     \param
+        fn: the function to minimize
+        x_k: current parameter vector x at k
+        p_k: search step direction
         alpha_max: bound on the maximum step length allowed; p60
 
     \return
@@ -14,6 +19,7 @@ def line_search_algorithm(fn, x_k, p_k, alpha_max):
     \notation
     * alpha: $\alpha_i$
     * prev_alpha: $\alpha_{i-1}$
+    * next_alpha: $\alpha_{i+1}$
     * phi_0: $\phi(0)$
     * phi_der_0: $\phi'(0)$
     '''
@@ -32,17 +38,18 @@ def line_search_algorithm(fn, x_k, p_k, alpha_max):
     i = 1
     while True:
         phi_alpha = phi(alpha)
+
         phi_prev_alpha = phi(prev_alpha)
         phi_0 = phi(0.0)
         phi_der_0 = util.grad(phi, torch.zeros(1))
 
-        if (phi_alpha > phi_0 + c1 * alpha * phi_der_0) or (phi_alpha >= phi_prev_alpha and i > 1):
+        if (phi_alpha > (phi_0 + c1 * alpha * phi_der_0)) or ((phi_alpha >= phi_prev_alpha) and (i > 1)):
             alpha_star = zoom(prev_alpha, alpha, phi, c1, c2)
             return alpha_star
 
         phi_der_alpha = util.grad(phi, torch.tensor([alpha]))
 
-        if abs(phi_der_alpha) <= -c2 * phi_der_0:
+        if abs(phi_der_alpha) <= (-c2 * phi_der_0):
             alpha_star = alpha
             return alpha_star
 
@@ -50,7 +57,8 @@ def line_search_algorithm(fn, x_k, p_k, alpha_max):
             alpha_star = zoom(alpha, prev_alpha, phi, c1, c2)
             return alpha_star
 
+        next_alpha = uniformly_random(alpha, alpha_max)
         prev_alpha = alpha
-        alpha = uniformly_random(alpha, alpha_max)
+        alpha = next_alpha
 
         i += 1
