001513cdf07124d8f3be2822ea138ece1c6c8078
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-07-22 20:42:25 +1000

    fix hvp

--- optim/numopt-nocedal-2006/script/util.py
+++ optim/numopt-nocedal-2006/script/util.py
@@ -12,10 +12,10 @@ def hess_vec_prod(fn, _x, v):
     # https://discuss.pytorch.org/t/calculating-hessian-vector-product/11240
     x = Variable(_x.data.clone(), requires_grad=True)
     out = fn(x)
-    grad_f, = torch.autograd.grad(out, x, create_graph=True)
-    z = torch.dot(grad_f, v)
-    z.backward()
-    return x.grad
+    grad, = torch.autograd.grad(out, x, create_graph=True)
+    prod = torch.dot(grad, v)
+    grad, = torch.autograd.grad(prod, x, create_graph=True)
+    return grad
 
 def rosenbrock(x):
     # https://en.wikipedia.org/wiki/Rosenbrock_function
