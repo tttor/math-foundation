a4775967cf91d495f2b9cc837dde285b4e66c7de
Vektor Dewanto <vektor.dewanto@gmail.com>
2019-08-22 08:11:17 +1000

    fix

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #git remote add upstream git@github.com:tttor/math-foundation.git
 
+echo '=== comitting ==='
+git pull
+git add --all
+git commit -a -m fix
+
 echo '=== github.com ==='
 git push upstream master
